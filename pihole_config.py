98b731afe78c7ba2cd403bfa5aca4570aa0a4cbf
Andreas Lindström <anteln@gmail.com>
2025-05-25 19:16:25 +0200

    Minor fixes

--- pihole_config.py
+++ pihole_config.py
@@ -3,23 +3,23 @@ import argparse
 
 import pihole6api
 
-def clear_lists(client):
+def clear_lists(client: pihole6api.PiHole6Client):
   lists = client.list_management.get_lists()
   for list in lists["lists"]:
     client.list_management.delete_list(list["address"], list["type"])
 
-def add_lists(client):
+def add_lists(client: pihole6api.PiHole6Client):
   for list in data["lists"]:
     client.list_management.add_list(list["address"], list["type"])
 
-def set_domain(client):
+def set_domain(client: pihole6api.PiHole6Client):
   config_changes = {}
   config_changes["dns"] = {}
   config_changes["dns"]["domain"] = data["domain"]
 
   client.config.update_config(config_changes)
 
-def add_hosts(client):
+def add_hosts(client: pihole6api.PiHole6Client):
   for host in data["hosts"]:
     client.config.add_local_a_record(".".join((host["name"], data["domain"])), host["address"])
 
